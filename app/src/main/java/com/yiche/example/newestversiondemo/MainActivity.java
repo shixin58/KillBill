1e0d6d57596adb2b3eb9432bf6724fba090ad94e
shixin <shixin58@163.com>
2017-12-14 16:45:43 +0800

    引入butterknife

--- app/src/main/java/com/yiche/example/newestversiondemo/MainActivity.java
+++ app/src/main/java/com/yiche/example/newestversiondemo/MainActivity.java
@@ -6,15 +6,23 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Chronometer;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-    private EditText mTextInput;
-    private Chronometer mChronometer;
+    @BindView(R.id.message)
+    TextView mTextMessage;
+    @BindView(R.id.input)
+    EditText mTextInput;
+    @BindView(R.id.chronometer)
+    Chronometer mChronometer;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -40,15 +48,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        mTextMessage = findViewById(R.id.message);
         mTextInput = findViewById(R.id.input);
         String input = getPreferences(Context.MODE_PRIVATE)
                 .getString("input", "");
         mTextInput.setText(input, TextView.BufferType.NORMAL);
         mChronometer = findViewById(R.id.chronometer);
-        mChronometer.start();
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
@@ -61,5 +70,10 @@ public class MainActivity extends AppCompatActivity {
                 .putString("input", input)
                 .apply();
         mChronometer.stop();
+    }
+
+    @OnClick(R.id.chronometer)
+    void onChronometerClick(View view){
+        ((Chronometer)view).start();
     }
 }
