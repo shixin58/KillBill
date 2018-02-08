3c86044ecc217f978667f6219b8bc2964bf7833d
shixin <shixin58@163.com>
2018-02-08 10:42:38 +0800

    测试PopupWindow

--- app/src/main/java/com/yiche/example/newestversiondemo/MainActivity.java
+++ app/src/main/java/com/yiche/example/newestversiondemo/MainActivity.java
@@ -5,12 +5,16 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Chronometer;
 import android.widget.EditText;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
+import butterknife.BindString;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -23,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
     EditText mTextInput;
     @BindView(R.id.chronometer)
     Chronometer mChronometer;
+    @BindString(R.string.app_name)
+    String name;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -76,6 +82,22 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.button)
     void onCreateBugClick(){
-        int i = 1/0;
+        //Log.i("Victor", ""+name);
+        //Toast.makeText(this, ""+name, Toast.LENGTH_LONG).show();
+        View popupView = LayoutInflater.from(this).inflate(R.layout.view_popup_window, null);
+        final PopupWindow popupWindow = new PopupWindow(popupView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+//        popupWindow.showAtLocation(mChronometer, Gravity.RIGHT|Gravity.BOTTOM, 200, 200);
+        popupView.findViewById(R.id.tv_welcome).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                popupWindow.dismiss();
+            }
+        });
+//        popupWindow.setTouchable(true);
+//        popupWindow.setOutsideTouchable(false);
+        popupWindow.showAsDropDown(mChronometer, 10, 10);
+//        if(Build.VERSION.SDK_INT>=21) {
+//            popupWindow.setElevation(5f);
+//        }
     }
 }
