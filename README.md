1ceda5294f7888816163d22aa1fce9da7f4e84b2
shixin <shixin58@163.com>
2018-09-01 19:15:44 +0800

    java注解，运行时通过反射获取

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 - demo - MD5算法
 - demo - DES加密算法
 - demo - java反射机制
+- demo - java注解
---
+++ app/src/main/java/com/victor/demon/repository/FineArt.java
@@ -0,0 +1,17 @@
+package com.victor.demon.repository;
+
+/**
+ * <p>Created by shixin on 2018/9/1.
+ */
+@Curriculum(value = "Math", age = 300)
+public class FineArt {
+
+    @Stone
+    public void doSculpture() {
+    }
+
+    @Deprecated
+    @Stone
+    public void doArchitecture(@Superman(height = 2.2f) String type) {
+    }
+}
