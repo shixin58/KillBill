f753630c8b31f6b1fd5cc0cb58c1fa6703f71d88
shixin <shixin58@163.com>
2018-09-07 10:27:09 +0800

    跨进程调用页面

--- client/src/main/java/com/max/client/MainActivity.java
+++ client/src/main/java/com/max/client/MainActivity.java
@@ -1,6 +1,9 @@
 package com.max.client;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 import com.max.baselib.BaseActivity;
 
@@ -9,10 +12,26 @@ import org.jetbrains.annotations.Nullable;
 /**
  * <p>Created by shixin on 2018/9/4.
  */
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements View.OnClickListener{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        findViewById(R.id.tv_title).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tv_title:
+                // app进程间调用页面
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("max://demon/recycler.view"));
+                startActivity(intent);
+                break;
+        }
     }
 }
