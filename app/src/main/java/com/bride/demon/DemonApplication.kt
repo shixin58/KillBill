add1e0baed72c65fcc6a0e1a23127091eaf560c6
shixin <shixin58@163.com>
2018-11-06 14:07:14 +0800

    分包

--- app/src/main/java/com/bride/demon/DemonApplication.kt
+++ app/src/main/java/com/bride/demon/DemonApplication.kt
@@ -1,9 +1,10 @@
 package com.bride.demon
 
 import android.app.Application
+import android.content.Context
+import androidx.multidex.MultiDex
 import com.bride.baselib.PreferenceUtils
 import com.bride.baselib.ResUtils
-
 import com.github.moduth.blockcanary.BlockCanary
 import com.squareup.leakcanary.LeakCanary
 
@@ -13,6 +14,11 @@ import com.squareup.leakcanary.LeakCanary
  */
 class DemonApplication : Application() {
     private val activityLifecycleCallbacks = MyActivityLifecycleCallbacks()
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        MultiDex.install(base)
+    }
 
     override fun onCreate() {
         super.onCreate()
