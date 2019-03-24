78b370b312cc707d4d40fd7b33181e4d18360360
shixin <shixin58@163.com>
2019-03-24 21:07:42 +0800

    review bitmap

--- app/src/main/java/com/bride/demon/activity/WebpActivity.java
+++ app/src/main/java/com/bride/demon/activity/WebpActivity.java
@@ -2,6 +2,8 @@ package com.bride.demon.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.AttributeSet;
@@ -12,58 +14,69 @@ import android.widget.ImageView;
 import com.bride.baselib.BaseActivity;
 import com.bride.demon.R;
 
-import org.jetbrains.annotations.Nullable;
-
 /**
  * <p>Created by shixin on 2018/10/22.
  */
 public class WebpActivity extends BaseActivity {
+    private static final String TAG = WebpActivity.class.getSimpleName();
     public static void openActivity(Context context) {
         Intent intent = new Intent(context, WebpActivity.class);
         context.startActivity(intent);
     }
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_webp);
         initView();
     }
 
     private void initView() {
-        ImageView imageView = findViewById(R.id.iv_example1);
-        // 关晓彤
-        imageView.setImageResource(R.mipmap.actress);
+        ImageView imageView = findViewById(R.id.iv_example);
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = 4;
+        options.inJustDecodeBounds = false;
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.actress, options);
+        Log.i(TAG, "size = "+options.outWidth+", "+options.outHeight+", "+(getBitmapSize(bitmap)/1024)+"KB");
+        Log.i(TAG, "outMimeType = "+options.outMimeType);// image/jpeg
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            Log.i(TAG, "outConfig = "+options.outConfig);// ARGB_8888
+            Log.i(TAG, "outColorSpace = "+options.outColorSpace);// sRGB IEC61966-2.1 (id=0, model=RGB)
+        }
+        imageView.setImageBitmap(bitmap);
+    }
+
+    private int getBitmapSize(Bitmap bitmap) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            return bitmap.getAllocationByteCount();
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR1) {
+            return bitmap.getByteCount();
+        } else {
+            return bitmap.getRowBytes() * bitmap.getHeight();
+        }
     }
 
     @Override
     public View onCreateView(String name, Context context, AttributeSet attrs) {
         View view = super.onCreateView(name, context, attrs);
-        Log.i("onCreateView", name+": "+view);
+        Log.i(TAG, "onCreateView - name["+name
+                +"] - view["+(view==null?null:view.getClass().getSimpleName())+"]");
         return view;
     }
 
     @Override
     public View onCreateView(View parent, String name, Context context, AttributeSet attrs) {
-        if(attrs!=null && attrs.getAttributeCount()>0) {
-            for(int i=0;i<attrs.getAttributeCount();i++) {
-                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                    Log.i("AttributeSet", attrs.getAttributeNamespace(i)+"-"+attrs.getAttributeName(i)+"-"+attrs.getAttributeValue(i));
-                }else {
-                    Log.i("AttributeSet", attrs.getAttributeName(i)+"-"+attrs.getAttributeValue(i));
-                }
-            }
-        }
         View view = super.onCreateView(parent, name, context, attrs);
-        Log.i("onCreateView1", parent+": "+view);
+        Log.i(TAG, "onCreateView1 - parent["+(parent==null?null:parent.getClass().getSimpleName())+"] - name["+name
+                +"] - view["+(view==null?null:view.getClass().getSimpleName())+"]");
         return view;
     }
 
-    @androidx.annotation.Nullable
     @Override
     public View onCreatePanelView(int featureId) {
         View view = super.onCreatePanelView(featureId);
-        Log.i("onCreatePanelView", featureId+": "+view);
+        Log.i(TAG, "onCreatePanelView - " + featureId
+                + " - view["+(view==null?null:view.getClass().getSimpleName())+"]");
         return view;
     }
 }
