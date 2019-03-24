caf4b81aab7c4cbeca50abb0cc698a89f5b6ea56
shixin <shixin58@163.com>
2019-03-24 17:04:35 +0800

    乘除法

--- client/src/main/java/com/bride/client/algorithm/OneNumClient.java
+++ client/src/main/java/com/bride/client/algorithm/OneNumClient.java
@@ -10,6 +10,8 @@ public class OneNumClient {
 
         System.out.println("oneNum "+oneNum(7));
         System.out.println("oneNumNew "+ oneNumNew(7));
+        System.out.println("multiply: " + multiply(6, 17));
+        System.out.println("divide: " + divide(Integer.MIN_VALUE, 1));
     }
 
     // 使用按位与和右移运算符，假定n > 0
@@ -29,5 +31,83 @@ public class OneNumClient {
             n = n & (n-1);
         }
         return count;
+    }
+
+    public static int multiply(int a, int b) {
+        int result = 0;
+        int count = 0;
+        while (b > 0) {
+            if ((b&1) == 1) {
+                result += (a << count);
+            }
+            count++;
+            b = b >> 1;
+        }
+        return result;
+    }
+
+    public static int divide(int dividend, int divisor) {
+        int quotient = 0;
+        if (divisor == Integer.MIN_VALUE) {
+            if (dividend == Integer.MIN_VALUE) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+        if (dividend == Integer.MIN_VALUE) {
+            if (divisor == -1) {
+                return Integer.MAX_VALUE;
+            } else if(divisor > 0){
+                dividend += divisor;
+                quotient--;
+            } else {
+                dividend -= divisor;
+                quotient++;
+            }
+        }
+        boolean positive = true;
+        if (dividend < 0 && divisor < 0) {
+            dividend = Math.abs(dividend);
+            divisor = Math.abs(divisor);
+        } else if (dividend < 0) {
+            dividend = Math.abs(dividend);
+            positive = false;
+        } else if (divisor < 0) {
+            divisor = Math.abs(divisor);
+            positive = false;
+        }
+
+        System.out.println("dividend = "+dividend+"; divisor = "+divisor);
+        if (dividend < divisor) {
+            return quotient;
+        }
+
+        // 求出被除数和除数最高位1所在位置
+        int divisorHighestPos = 0;
+        int dividendHighestPos = 0;
+        int tmp = divisor;
+        while (tmp > 0) {
+            divisorHighestPos++;
+            tmp = tmp >> 1;
+        }
+        tmp = dividend;
+        while (tmp > 0) {
+            dividendHighestPos++;
+            tmp = tmp >> 1;
+        }
+        System.out.println("dividendHighestPos = "+dividendHighestPos+"; divisorHighestPos = "+divisorHighestPos);
+
+        int tmpQuotient = 0;
+        int interval = dividendHighestPos - divisorHighestPos;
+        while (dividend >= divisor && interval >= 0) {
+            if (dividend >= (divisor << interval)) {
+                dividend -= (divisor << interval);
+                tmpQuotient += (1<<interval);
+            }
+            interval--;
+        }
+        quotient += positive?tmpQuotient:-tmpQuotient;
+        return quotient;
     }
 }
