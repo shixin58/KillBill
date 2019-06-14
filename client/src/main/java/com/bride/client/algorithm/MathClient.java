89920332e5f7f77a3f91e148ae8e79927c60b1d3
shixin58 <shixin1359@gmail.com>
2019-06-14 18:57:33 +0800

    Math.round, System.arraycopy, Arrays.deepEquals

--- client/src/main/java/com/bride/client/algorithm/MathClient.java
+++ client/src/main/java/com/bride/client/algorithm/MathClient.java
@@ -10,6 +10,7 @@ public class MathClient {
 
     public static void main(String[] args) {
         testRandom();
+        testMath();
     }
 
     public static void testRandom() {
@@ -23,5 +24,13 @@ public class MathClient {
         byte[] buf = new byte[16];
         random.nextBytes(buf);
         System.out.println("byte[]: "+Arrays.toString(buf));
+    }
+
+    public static void testMath() {
+        float origin = -3.1415f;
+        int result = (int) Math.floor(origin+0.5f);
+        // 小数位四舍五入
+        int terminal = Math.round(origin);
+        System.out.printf("result = %d, terminal = %d", result, terminal);
     }
 }
--- client/src/main/java/com/bride/client/reflect/ArrayClient.java
+++ client/src/main/java/com/bride/client/reflect/ArrayClient.java
@@ -1,13 +1,14 @@
 package com.bride.client.reflect;
 
 import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * <p>Created by shixin on 2019/3/18.
  */
 public class ArrayClient {
+
     public static void main(String[] args) {
         // 反射Array
         Class<?> stringClass = null;
@@ -34,9 +35,54 @@ public class ArrayClient {
         names[2] = "Mexico City";
         System.out.println(Arrays.toString(names));
 
-        Object[] objects = {"Victor", "Jacob", "Max"};
-        objects = names;
-        // 类型擦除，Object[]
-        ArrayList<String> arrayList = new ArrayList<>();
+        polymorphic();
+
+        copy();
+
+        order();
+    }
+
+    public static void polymorphic() {
+        Object[] objects0 = {"Victor", "Jacob", "Max"};// Object数组
+        Object[] objects = new String[]{"Victor", "Jacob", "Max"};// String 数组
+        System.out.println(objects0+" "+objects);
+        try {
+            objects[0] = Integer.valueOf(3);
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+
+        Individual[] individuals = new Human[2];
+        try {
+            individuals[0] = new Animal();
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+    }
+
+    public static void copy() {
+        // java.lang.ArrayStoreException
+        Integer[] integers = {3, 5, 8, 7};
+        Integer[] integers1 = new Integer[integers.length * 2];
+        System.arraycopy(integers, 0, integers1, 0, integers.length);
+        System.out.printf("integers = %s, integers1 = %s\n", Arrays.toString(integers), Arrays.toString(integers1));
+
+        String[][] animals = {{"crow", "pigeon"}, {"swordfish"}};
+        Object[][] objects = {{"crow", "pigeon"}, {"swordfish", "squid"}};
+        System.out.println("deepEquals: "+Arrays.deepEquals(animals, objects));
+    }
+
+    public static void order() {
+        Integer[] integers = {3, 1, 5, 4};
+        // 泛型
+        Arrays.sort(integers, Collections.reverseOrder());
+        Arrays.sort(integers);
+        System.out.println(Arrays.toString(integers));
+        // 未找到，返回位置为 -(插入点) -1
+        System.out.printf("position of 3 = %d, position of 8 = %d\n", Arrays.binarySearch(integers, 3), Arrays.binarySearch(integers, 2));
+
+        String[] strings = {"banana", "apple", "orange"};
+        Arrays.sort(strings, String.CASE_INSENSITIVE_ORDER);
+        System.out.println(Arrays.toString(strings));
     }
 }
