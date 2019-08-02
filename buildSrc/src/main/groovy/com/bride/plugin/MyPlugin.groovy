843e652d2d07a81612faf6943840a7b52d13a92b
shixin58 <shixin1359@gmail.com>
2019-08-02 10:07:58 +0800

    自定义插件

---
+++ buildSrc/src/main/groovy/com/bride/plugin/MyPlugin.groovy
@@ -0,0 +1,14 @@
+package com.bride.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+public class MyPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+        project.task("greetings") {
+            println("What's the weather like today?")
+        }
+    }
+}
