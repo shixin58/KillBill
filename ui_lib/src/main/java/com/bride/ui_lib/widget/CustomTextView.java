1b5bccb6e2598fe36f64feedaa65d309d464b335
shixin58 <shixin1359@gmail.com>
2019-08-23 11:21:00 +0800

    测试measure

--- ui_lib/src/main/java/com/bride/ui_lib/widget/CustomTextView.java
+++ ui_lib/src/main/java/com/bride/ui_lib/widget/CustomTextView.java
@@ -10,6 +10,7 @@ import androidx.appcompat.widget.AppCompatTextView;
  * <p>Created by shixin on 2019-07-31.
  */
 public class CustomTextView extends AppCompatTextView {
+    private static final String TAG = CustomTextView.class.getSimpleName();
 
     public CustomTextView(Context context) {
         super(context);
@@ -21,9 +22,16 @@ public class CustomTextView extends AppCompatTextView {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        Log.i("CustomTextView", MeasureSpec.getMode(heightMeasureSpec)+" "+MeasureSpec.getSize(heightMeasureSpec));
+        Log.i(TAG, hashCode()+"# onMeasure "+MeasureSpec.getMode(widthMeasureSpec)+" "+MeasureSpec.getSize(widthMeasureSpec)
+                +" "+MeasureSpec.getMode(heightMeasureSpec)+" "+MeasureSpec.getSize(heightMeasureSpec));
         // WRAP_CONTENT: UNSPECIFIED 1560
         // 40dp: EXACTLY 120
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        Log.i(TAG, hashCode()+"# onLayout "+changed+" "+left+" "+top+" "+right+" "+bottom);
     }
 }
