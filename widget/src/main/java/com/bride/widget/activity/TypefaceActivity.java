0f54cb0d0ade919569c3bbd49ab71e7f16a5bd4d
shixin <shixin1359@gmail.com>
2020-07-15 17:19:05 +0800

    加动态转盘

--- widget/src/main/java/com/bride/widget/activity/TypefaceActivity.java
+++ widget/src/main/java/com/bride/widget/activity/TypefaceActivity.java
@@ -2,6 +2,10 @@ package com.bride.widget.activity;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.TextView;
 
 import com.bride.ui_lib.BaseActivity;
@@ -19,5 +23,24 @@ public class TypefaceActivity extends BaseActivity {
         TextView textView = findViewById(R.id.tv_brain);
         Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/Arial_Rounded_Bold.ttf");
         textView.setTypeface(tf);
+    }
+
+    private void setRoulette() {
+        RotateAnimation ra = new RotateAnimation(0, 360,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        ra.setDuration(2000L);
+        ra.setFillAfter(true);
+        ra.setInterpolator(new LinearInterpolator());
+        ra.setRepeatMode(Animation.RESTART);
+        ra.setRepeatCount(Animation.INFINITE);
+        findViewById(R.id.container_roulette).startAnimation(ra);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tv_brain:
+                setRoulette();
+                break;
+        }
     }
 }
