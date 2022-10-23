1b7ba1227c81a03060d4332edc270ee7612b1d54
shixin <shixin1359@gmail.com>
2022-10-23 11:47:55 +0800

    关闭leakcanary, 更新签名, 全面支持Kotlin

--- client/src/main/java/com/bride/client/activity/BinderActivity.java
+++ client/src/main/java/com/bride/client/activity/BinderActivity.java
@@ -11,9 +11,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.bride.client.databinding.ActivityBinderBinding;
 import com.bride.ui_lib.BaseActivity;
 import com.bride.client.BuildConfig;
-import com.bride.client.R;
 import com.bride.demon.Form;
 import com.bride.demon.IMyService;
 
@@ -21,7 +21,7 @@ import com.bride.demon.IMyService;
  * <p>Created by shixin on 2019-05-15.
  */
 public class BinderActivity extends BaseActivity {
-    private static final String TAG = BinderActivity.class.getSimpleName();
+    private ActivityBinderBinding mBinding;
 
     private IMyService mService;
 
@@ -33,43 +33,41 @@ public class BinderActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_binder);
+        mBinding = ActivityBinderBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
     }
 
     public void onClick(View v) {
         Intent intent;
-        switch (v.getId()) {
-            case R.id.tv_switch:
-                if(mService == null) {
-                    try {
-                        // 启动服务并拿到服务代理。服务所在进程必须运行
-                        intent = new Intent();
-                        intent.setClassName("com.bride.demon" + (BuildConfig.DEBUG ? ".debug":""),
-                                "com.bride.demon.service.MyService");
-                        bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
-                    } catch (Exception e) {
-                        // java.lang.SecurityException: Not allowed to bind to service Intent { cmp=com.bride.demon.debug/com.bride.demon.service.MyService }
-                        e.printStackTrace();
-                    }
-                }else {
-                    // IllegalArgumentException: Service not registered: com.bride.client.MainActivity$3@dc78e3b
-                    unbindService(mServiceConnection);
-                    mService = null;
+        if (v == mBinding.tvSwitch) {
+            if(mService == null) {
+                try {
+                    // 启动服务并拿到服务代理。服务所在进程必须运行
+                    intent = new Intent();
+                    intent.setClassName("com.bride.demon" + (BuildConfig.DEBUG ? ".debug":""),
+                            "com.bride.demon.service.MyService");
+                    bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
+                } catch (Exception e) {
+                    // java.lang.SecurityException: Not allowed to bind to service Intent { cmp=com.bride.demon.debug/com.bride.demon.service.MyService }
+                    e.printStackTrace();
                 }
-                break;
-            case R.id.tv_use_service:
-                if(mService != null) {
-                    try {
-                        mService.basicTypes(1, 10L, true, 2.3f, 9.99, "Jacob");
-                        Log.i("IMyService", mService.getUser().toString());
-                        Log.i("IMyService", "add "+mService.add(1, 2));
-                        Log.i("IMyService", mService.getValue());
-                        mService.sendForm(new Form("洛杉矶", "1988"));
-                    } catch (RemoteException e) {
-                        e.printStackTrace();
-                    }
+            }else {
+                // IllegalArgumentException: Service not registered: com.bride.client.MainActivity$3@dc78e3b
+                unbindService(mServiceConnection);
+                mService = null;
+            }
+        } else if (v == mBinding.tvUseService) {
+            if(mService != null) {
+                try {
+                    mService.basicTypes(1, 10L, true, 2.3f, 9.99, "Jacob");
+                    Log.i("IMyService", mService.getUser().toString());
+                    Log.i("IMyService", "add "+mService.add(1, 2));
+                    Log.i("IMyService", mService.getValue());
+                    mService.sendForm(new Form("洛杉矶", "1988"));
+                } catch (RemoteException e) {
+                    e.printStackTrace();
                 }
-                break;
+            }
         }
     }
 
