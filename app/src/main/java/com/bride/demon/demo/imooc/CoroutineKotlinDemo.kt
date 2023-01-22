8537c6fe95b6bbaa66cd793b40358b61337e19df
shixin <shixin1359@gmail.com>
2023-01-22 18:07:13 +0800

    仿Python Generator, 抄sequence

--- app/src/main/java/com/bride/demon/demo/imooc/CoroutineKotlinDemo.kt
+++ app/src/main/java/com/bride/demon/demo/imooc/CoroutineKotlinDemo.kt
@@ -1,10 +1,7 @@
 package com.bride.demon.demo.imooc
 
 import kotlinx.coroutines.delay
-import kotlin.coroutines.Continuation
-import kotlin.coroutines.CoroutineContext
-import kotlin.coroutines.EmptyCoroutineContext
-import kotlin.coroutines.startCoroutine
+import kotlin.coroutines.*
 
 fun main() {
     // 标准库序列生成器
@@ -30,4 +27,99 @@ fun main() {
 
         override fun resumeWith(result: Result<Unit>) {}
     })
+
+    val nums = generator<Int> { start ->
+        for (i in 0..5) {
+            yield(start + i)
+        }
+    }
+
+    val sequence = nums(10)
+
+    for (i in sequence) {
+        println(i)
+    }
+}
+
+interface Generator<T> {
+    operator fun iterator(): Iterator<T>
+}
+
+class GeneratorImpl<T>(private val block: suspend GeneratorScope<T>.(T) -> Unit, private val param: T): Generator<T> {
+    override fun iterator(): Iterator<T> {
+        return GeneratorIterator(block, param)
+    }
+}
+
+sealed class State {
+    class NotReady(val continuation: Continuation<Unit>): State()
+    class Ready<T>(val continuation: Continuation<Unit>, val nextValue: T): State()
+    object Done: State()
+}
+
+class GeneratorIterator<T>(private val block: suspend GeneratorScope<T>.(T) -> Unit, override val param: T)
+    : GeneratorScope<T>(), Iterator<T>, Continuation<Any?> {
+    override val context: CoroutineContext = EmptyCoroutineContext
+
+    private var state: State
+
+    init {
+        val coroutineBlock: suspend GeneratorScope<T>.() -> Unit = { block(param) }
+        val start = coroutineBlock.createCoroutine(this, this)
+        state = State.NotReady(start)
+    }
+
+    private fun resume() {
+        when(val currentState = state) {
+            is State.NotReady -> currentState.continuation.resume(Unit)
+            is State.Ready<*> -> {}
+            State.Done -> {}
+        }
+    }
+
+    override fun hasNext(): Boolean {
+        resume()
+        return state != State.Done
+    }
+
+    override fun next(): T {
+        return when(val currentState = state) {
+            is State.NotReady -> {
+                resume()
+                next()
+            }
+            is State.Ready<*> -> {
+                state = State.NotReady(currentState.continuation)
+                (currentState as State.Ready<T>).nextValue
+            }
+            State.Done -> {
+                throw IndexOutOfBoundsException("No value left.")
+            }
+        }
+    }
+
+    override suspend fun yield(value: T) = suspendCoroutine<Unit> { continuation ->
+        state = when(state) {
+            is State.NotReady -> State.Ready(continuation, value)
+            State.Done -> throw IllegalStateException("Cannot yield a value when done.")
+            is State.Ready<*> -> throw IllegalStateException("Cannot yield a value when ready.")
+        }
+    }
+
+    override fun resumeWith(result: Result<Any?>) {
+        state = State.Done
+        result.getOrThrow()
+    }
+}
+
+abstract class GeneratorScope<T> internal constructor() {
+    protected abstract val param: T
+
+    abstract suspend fun yield(value: T)
+}
+
+fun <T> generator(block: suspend GeneratorScope<T>.(T) -> Unit): (T) -> Generator<T> {
+    return { param ->
+        GeneratorImpl(block, param)
+    }
 }
