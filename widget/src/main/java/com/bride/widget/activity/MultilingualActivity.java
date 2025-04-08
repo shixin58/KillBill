d25790f0a591880f4007f3470e56f5e7540d623f
shixin <shixin1359@gmail.com>
2025-04-08 09:29:10 +0800

    测试字符宽度

--- widget/src/main/java/com/bride/widget/activity/MultilingualActivity.java
+++ widget/src/main/java/com/bride/widget/activity/MultilingualActivity.java
@@ -1,10 +1,15 @@
 package com.bride.widget.activity;
 
+import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import com.bride.baselib.Constants;
+import com.bride.baselib.LogUtils;
 import com.bride.baselib.PreferenceUtils;
 import com.bride.ui_lib.BaseActivity;
 import com.bride.widget.R;
@@ -19,9 +24,24 @@ public class MultilingualActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multilingual);
 
+        checkLang();
+        testCharWidth();
+    }
+
+    private void checkLang() {
         RadioGroup rg = findViewById(R.id.rg_language);
-        int lang = PreferenceUtils.getInt(Constants.KEY_LANG, Constants.LANG_ENGLISH);
-        checkLang(rg, lang);
+        int langInt = PreferenceUtils.getInt(Constants.KEY_LANG, Constants.LANG_ENGLISH);
+        if (langInt == Constants.LANG_SIMPLIFIED_CHINESE) {
+            rg.check(R.id.rb_chinese);
+        } else if (langInt == Constants.LANG_TRADITIONAL_CHINESE) {
+            rg.check(R.id.rb_chinese_traditional);
+        } else if (langInt == Constants.LANG_KOREAN) {
+            rg.check(R.id.rb_korean);
+        } else if (langInt == Constants.LANG_SPANISH) {
+            rg.check(R.id.rb_spanish);
+        }else {
+            rg.check(R.id.rb_english);
+        }
         rg.setOnCheckedChangeListener((group, checkedId) -> {
             // Switch statement can be replaced with enhanced 'switch'
             if (checkedId == R.id.rb_chinese) {
@@ -38,18 +58,22 @@ public class MultilingualActivity extends BaseActivity {
         });
     }
 
-    private void checkLang(RadioGroup rg, int langInt) {
-        if (langInt == Constants.LANG_SIMPLIFIED_CHINESE) {
-            rg.check(R.id.rb_chinese);
-        } else if (langInt == Constants.LANG_TRADITIONAL_CHINESE) {
-            rg.check(R.id.rb_chinese_traditional);
-        } else if (langInt == Constants.LANG_KOREAN) {
-            rg.check(R.id.rb_korean);
-        } else if (langInt == Constants.LANG_SPANISH) {
-            rg.check(R.id.rb_spanish);
-        }else {
-            rg.check(R.id.rb_english);
+    private void testCharWidth() {
+        Paint p = new Paint();
+        p.setTextSize(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 16f, getResources().getDisplayMetrics()));
+        p.setTypeface(Typeface.DEFAULT);
+
+        TextView tvHelloExtra = findViewById(R.id.tv_hello_extra);
+        String[] helloArr = getResources().getStringArray(R.array.spanish);
+        StringBuilder helloBuilder = new StringBuilder();
+        for (String s : helloArr) {
+            LogUtils.INSTANCE.i("MultilingualActivity", s + ": " + p.measureText(s));
+            helloBuilder.append(s).append('\n');
         }
+        if (helloBuilder.length() > 0) {
+            helloBuilder.deleteCharAt(helloBuilder.length() - 1);
+        }
+        tvHelloExtra.setText(helloBuilder.toString());
     }
 
     public void onClick(View v) {
